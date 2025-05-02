51f3fe1be6f1eb95cdfddafcf4fdbdccc91775ed
fahernkhan <fathurrahmanhernanda123@gmail.com>
2025-05-02 20:35:40 +0700

    fix to can curl from all device

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = Number(process.env.APP_PORT) || 3000; // pastikan number
+const port = process.env.APP_PORT || 3000;
 
 app.use(express.json());
 app.use(`/api/v1/auth`, authRoutes);
 app.use(`/api/v1/item`, itemRoutes);
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
